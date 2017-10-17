0dba1c9e0b790f9f88e8f3abed21a130dfd61f74
Andrew Romanov <scalli-k-be@ya.ru>
2017-10-17 15:49:04 +0700

    added code to handle keyboard notifications

--- KeyboardNotifications/ViewController.m
+++ KeyboardNotifications/ViewController.m
@@ -10,20 +10,157 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UITextField* textField;
+
+- (IBAction)compliteEdition:(id)sender;
+
 @end
+
+
+@interface ViewController (Private)
+
+- (void)_printNotificationInformation:(NSNotification*)notification;
+
+@end
+
+
+@interface ViewController (Notifications)
+
+- (void)_subscribeToNotifications;
+- (void)_unsubscribeFromNotifications;
+- (void)_UIKeyboardWillShowNotification:(NSNotification*)notification;
+- (void)_UIKeyboardDidShowNotification:(NSNotification*)notification;
+- (void)_UIKeyboardWillHideNotification:(NSNotification*)notification;
+- (void)_UIKeyboardDidHideNotification:(NSNotification*)notification;
+- (void)_UIKeyboardWillChangeFrameNotification:(NSNotification*)notification;
+- (void)_UIKeyboardDidChangeFrameNotification:(NSNotification*)notification;
+
+- (void)_unsubscribeFromNotificationsInList:(NSArray<NSNotificationName>*)notifications;
+
+@end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	[self _subscribeToNotifications];
 }
 
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+#pragma mark Actions
+- (IBAction)compliteEdition:(id)sender
+{
+	[self.textField resignFirstResponder];
 }
 
+
+- (void)dealloc
+{
+	[self _unsubscribeFromNotifications];
+}
+
+@end
+
+
+#pragma mark -
+@implementation ViewController (Private)
+
+- (void)_printNotificationInformation:(NSNotification*)notification
+{
+	NSLog(@"Notification withName: %@ , \n userInfo: %@ \n***************", notification.name, notification.userInfo);
+}
+
+@end
+
+
+#pragma mark -
+@implementation ViewController (Notifications)
+
+- (void)_subscribeToNotifications
+{
+	[self _unsubscribeFromNotifications];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_UIKeyboardWillShowNotification:)
+																							 name:UIKeyboardWillShowNotification
+																						 object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+																					 selector:@selector(_UIKeyboardDidShowNotification:)
+																							 name:UIKeyboardDidShowNotification
+																						 object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+																					 selector:@selector(_UIKeyboardWillHideNotification:)
+																							 name:UIKeyboardWillHideNotification
+																						 object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+																					 selector:@selector(_UIKeyboardDidHideNotification:)
+																							 name:UIKeyboardDidHideNotification
+																						 object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+																					 selector:@selector(_UIKeyboardWillChangeFrameNotification:)
+																							 name:UIKeyboardWillChangeFrameNotification
+																						 object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self
+																					 selector:@selector(_UIKeyboardDidChangeFrameNotification:)
+																							 name:UIKeyboardDidChangeFrameNotification
+																						 object:nil];
+}
+
+
+- (void)_unsubscribeFromNotifications
+{
+	NSArray<NSNotificationName>* notifications = @[UIKeyboardWillShowNotification,
+																								 UIKeyboardDidShowNotification,
+																								 UIKeyboardWillHideNotification,
+																								 UIKeyboardDidHideNotification,
+																								 UIKeyboardWillChangeFrameNotification,
+																								 UIKeyboardDidChangeFrameNotification];
+	[self _unsubscribeFromNotificationsInList:notifications];
+}
+
+
+- (void)_UIKeyboardWillShowNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_UIKeyboardDidShowNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_UIKeyboardWillHideNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_UIKeyboardDidHideNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_UIKeyboardWillChangeFrameNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_UIKeyboardDidChangeFrameNotification:(NSNotification*)notification
+{
+	[self _printNotificationInformation:notification];
+}
+
+
+- (void)_unsubscribeFromNotificationsInList:(NSArray<NSNotificationName>*)notifications
+{
+	[notifications enumerateObjectsUsingBlock:^(NSNotificationName  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+		[[NSNotificationCenter defaultCenter] removeObserver:self name:obj object:nil];
+	}];
+}
 
 @end
